a836fde956adeae7d7b8f0e62f4afc0eb50572df
Emerson MX <emersonmx@gmail.com>
2026-06-11 17:58:58 -0300

    refactor: use bevy math

--- src/main.rs
+++ src/main.rs
@@ -1,51 +1,12 @@
-use bevy::{prelude::*, sprite::Anchor, window::WindowResolution};
+use bevy::{
+    math::bounding::{Aabb2d, BoundingVolume, IntersectsVolume},
+    prelude::*,
+    window::WindowResolution,
+};
 use bevy_inspector_egui::{bevy_egui::EguiPlugin, quick::WorldInspectorPlugin};
 use rand::{RngExt, rngs::ChaCha8Rng};
 
-#[derive(Debug, Default, Copy, Clone, Reflect)]
-struct Aabb {
-    left: f32,
-    right: f32,
-    top: f32,
-    bottom: f32,
-}
-
-impl Aabb {
-    fn new(center: Vec2, size: Vec2) -> Self {
-        let half_size = size / 2.0;
-        Self {
-            left: center.x - half_size.x,
-            right: center.x + half_size.x,
-            top: center.y + half_size.y,
-            bottom: center.y - half_size.y,
-        }
-    }
-
-    fn center(&self) -> Vec2 {
-        Vec2::new(
-            (self.left + self.right) / 2.0,
-            (self.top + self.bottom) / 2.0,
-        )
-    }
-
-    fn size(&self) -> Vec2 {
-        Vec2::new(self.right - self.left, self.top - self.bottom)
-    }
-
-    fn half_size(&self) -> Vec2 {
-        self.size() / 2.0
-    }
-
-    fn intersects(&self, other: &Aabb) -> bool {
-        self.left < other.right
-            && self.right > other.left
-            && self.top > other.bottom
-            && self.bottom < other.top
-    }
-}
-
 const PADDLE_SIZE: Vec2 = Vec2::new(20.0, 100.0);
-const PADDLE_OFFSET: f32 = 10.0;
 const BALL_SIZE: Vec2 = Vec2::new(10.0, 10.0);
 
 #[derive(Debug, Default, Component, Reflect, Deref, DerefMut)]
@@ -57,8 +18,11 @@ struct Speed(f32);
 #[derive(Debug, Default, Component, Reflect, Deref, DerefMut)]
 struct Velocity(Vec3);
 
-#[derive(Debug, Component, Deref, DerefMut)]
-struct CollisionRect(Aabb);
+#[derive(Debug, Default, Component, Reflect, Deref, DerefMut)]
+struct CollisionRect(Rectangle);
+
+#[derive(Debug, Default, Component, Reflect, Deref, DerefMut)]
+struct CollisionPlane(Plane2d);
 
 #[derive(Debug, Default, Component, Reflect, Deref, DerefMut)]
 struct LaunchTimer(Timer);
@@ -78,14 +42,17 @@ struct Paddle;
 #[derive(Component, Reflect)]
 struct Ball;
 
-#[derive(Debug, Component, Reflect)]
-struct Wall(Plane2d);
+#[derive(Component, Reflect)]
+struct Wall;
+
+#[derive(Component, Reflect)]
+struct RespawnBallArea;
 
 #[derive(Component, Reflect)]
 struct ScoreText;
 
 #[derive(Component, Reflect)]
-struct NeedsReset;
+struct NeedsRespawn;
 
 #[derive(Component, Reflect)]
 struct NeedsLaunch;
@@ -96,7 +63,7 @@ struct Dirty;
 #[derive(Debug, Resource, Reflect)]
 #[reflect(Resource)]
 struct Game {
-    area: Aabb,
+    area: Rectangle,
     score: (u32, u32),
     default_paddle_speed: f32,
     default_ball_speed: f32,
@@ -112,24 +79,37 @@ fn setup(game: Res<Game>, mut commands: Commands) {
 
     commands.spawn((
         Name::new("MiddleLine"),
-        Transform::from_translation(game.area.center().extend(0.0)),
+        Transform::from_xyz(0.0, 0.0, 0.0),
         Sprite::from_color(Color::WHITE, Vec2::new(1.0, game.area.size().y)),
     ));
 
+    let wall_offset = game.area.half_size.y;
     commands.spawn((
         Name::new("TopWall"),
-        Wall(Plane2d::new(Vec2::X)),
-        Transform::from_xyz(game.area.center().x, game.area.top, 0.0),
-        Sprite::from_color(Color::WHITE, Vec2::new(game.area.size().x, 2.0)),
+        Wall,
+        CollisionPlane(Plane2d::new(Vec2::NEG_Y)),
+        Transform::from_xyz(0.0, wall_offset, 0.0),
     ));
     commands.spawn((
         Name::new("BottomWall"),
-        Wall(Plane2d::new(Vec2::X)),
-        Transform::from_xyz(game.area.center().x, game.area.bottom, 0.0),
-        Sprite::from_color(Color::WHITE, Vec2::new(game.area.size().x, 2.0)),
+        Wall,
+        CollisionPlane(Plane2d::new(Vec2::Y)),
+        Transform::from_xyz(0.0, -wall_offset, 0.0),
     ));
 
-    let left_position = Vec3::new(game.area.left + PADDLE_OFFSET, 0.0, 0.0);
+    let respawn_offset = game.area.half_size.x;
+    commands.spawn((
+        Name::new("LeftRespawn"),
+        RespawnBallArea,
+        Transform::from_xyz(-respawn_offset, 0.0, 0.0),
+    ));
+    commands.spawn((
+        Name::new("RightRespawn"),
+        RespawnBallArea,
+        Transform::from_xyz(respawn_offset, 0.0, 0.0),
+    ));
+
+    let paddle_offset = game.area.half_size.x - 20.0;
     commands.spawn((
         Name::new("LeftPaddle"),
         Paddle,
@@ -139,33 +119,29 @@ fn setup(game: Res<Game>, mut commands: Commands) {
         },
         Speed(game.default_paddle_speed),
         Direction::default(),
-        CollisionRect(Aabb::new(left_position.truncate(), PADDLE_SIZE)),
-        Anchor::CENTER_LEFT,
-        Transform::from_translation(left_position),
+        CollisionRect(Rectangle::from_size(PADDLE_SIZE)),
+        Transform::from_xyz(-paddle_offset, 0.0, 0.0),
         Sprite::from_color(Color::WHITE, PADDLE_SIZE),
     ));
 
-    let right_position = Vec3::new(game.area.right - PADDLE_OFFSET, 0.0, 0.0);
     commands.spawn((
         Name::new("RightPaddle"),
         Paddle,
         Bot,
         Speed(game.default_paddle_speed),
         Direction::default(),
-        CollisionRect(Aabb::new(right_position.truncate(), PADDLE_SIZE)),
-        Anchor::CENTER_RIGHT,
-        Transform::from_translation(right_position),
+        CollisionRect(Rectangle::from_size(PADDLE_SIZE)),
+        Transform::from_xyz(paddle_offset, 0.0, 0.0),
         Sprite::from_color(Color::WHITE, PADDLE_SIZE),
     ));
 
-    let ball_position = game.area.center();
     commands.spawn((
         Name::new("Ball"),
         Ball,
         Speed(game.default_ball_speed),
-        NeedsReset,
-        CollisionRect(Aabb::new(ball_position, BALL_SIZE)),
-        Transform::from_translation(ball_position.extend(0.0)),
+        NeedsRespawn,
+        CollisionRect(Rectangle::from_size(BALL_SIZE)),
+        Transform::from_xyz(0.0, 0.0, 0.0),
         Sprite::from_color(Color::WHITE, BALL_SIZE),
     ));
 
@@ -191,9 +167,9 @@ fn setup(game: Res<Game>, mut commands: Commands) {
 
 fn paddle_input(
     keyboard_input: Res<ButtonInput<KeyCode>>,
-    mut query: Query<(&PaddleKeyboardInput, &mut Direction), With<Paddle>>,
+    mut inputs: Query<(&PaddleKeyboardInput, &mut Direction), With<Paddle>>,
 ) {
-    for (input, mut direction) in &mut query {
+    for (input, mut direction) in &mut inputs {
         direction.0 = Vec2::ZERO;
 
         if keyboard_input.pressed(input.up_key) {
@@ -206,17 +182,16 @@ fn paddle_input(
 }
 
 fn bot_input(
-    game: Res<Game>,
-    ball_query: Query<&Transform, With<Ball>>,
-    mut paddle_query: Query<(&Transform, &mut Direction), (With<Paddle>, With<Bot>)>,
+    balls: Query<&Transform, With<Ball>>,
+    mut paddles: Query<(&Transform, &mut Direction), (With<Paddle>, With<Bot>)>,
 ) {
-    let area_center = game.area.center();
+    let area_center = Vec2::ZERO;
     let epsilon = 10.0;
 
-    for ball_transform in &ball_query {
+    for ball_transform in &balls {
         let ball_pos = ball_transform.translation.truncate();
 
-        for (paddle_transform, mut direction) in &mut paddle_query {
+        for (paddle_transform, mut direction) in &mut paddles {
             direction.0 = Vec2::ZERO;
 
             let paddle_pos = paddle_transform.translation.truncate();
@@ -247,37 +222,36 @@ fn close_on_esc(
 fn update_score_text(
     game: Res<Game>,
     mut commands: Commands,
-    mut query: Query<(Entity, &mut Text), (With<ScoreText>, With<Dirty>)>,
+    mut scores: Query<(Entity, &mut Text), (With<ScoreText>, With<Dirty>)>,
 ) {
     let score = format!("{}   {}", game.score.0, game.score.1);
-    for (entity, mut text) in &mut query {
+    for (entity, mut text) in &mut scores {
         *text = Text::new(&score);
         commands.entity(entity).remove::<Dirty>();
     }
 }
 
-fn reset_ball(
+fn respawn_ball(
     mut commands: Commands,
-    mut query: Query<(Entity, &mut Transform), (With<Ball>, With<NeedsReset>)>,
-    game: Res<Game>,
+    mut balls: Query<(Entity, &mut Transform), (With<Ball>, With<NeedsRespawn>)>,
 ) {
-    for (entity, mut transform) in &mut query {
-        transform.translation = game.area.center().extend(0.0);
+    for (entity, mut transform) in &mut balls {
+        transform.translation = Vec3::ZERO;
 
         commands.entity(entity).remove::<Velocity>();
         commands
             .entity(entity)
             .insert(LaunchTimer(Timer::from_seconds(1.0, TimerMode::Once)));
-        commands.entity(entity).remove::<NeedsReset>();
+        commands.entity(entity).remove::<NeedsRespawn>();
     }
 }
 
 fn wait_ball_launch_timer(
     time: Res<Time>,
     mut commands: Commands,
-    mut query: Query<(Entity, &mut LaunchTimer), With<Ball>>,
+    mut balls: Query<(Entity, &mut LaunchTimer), With<Ball>>,
 ) {
-    for (entity, mut timer) in &mut query {
+    for (entity, mut timer) in &mut balls {
         if timer.tick(time.delta()).just_finished() {
             commands.entity(entity).insert(NeedsLaunch);
             commands.entity(entity).remove::<LaunchTimer>();
@@ -289,9 +263,9 @@ fn launch_ball(
     game: Res<Game>,
     mut rng: ResMut<GameRng>,
     mut commands: Commands,
-    mut query: Query<(Entity, &mut Speed), (With<Ball>, With<NeedsLaunch>)>,
+    mut balls: Query<(Entity, &mut Speed), (With<Ball>, With<NeedsLaunch>)>,
 ) {
-    for (entity, mut speed) in &mut query {
+    for (entity, mut speed) in &mut balls {
         let dir_x = if rng.random() { 1.0 } else { -1.0 };
         let dir_y = rng.random::<f32>() * 2.0 - 1.0;
         let new_dir = Vec3::new(dir_x, dir_y, 0.0).normalize();
@@ -304,71 +278,78 @@ fn launch_ball(
 
 fn paddle_movement(
     time: Res<Time<Fixed>>,
-    mut query: Query<(&Speed, &Direction, &mut Transform), With<Paddle>>,
+    walls: Query<(&CollisionPlane, &Transform), (With<Wall>, Without<Paddle>)>,
+    mut paddles: Query<(&CollisionRect, &Speed, &Direction, &mut Transform), With<Paddle>>,
 ) {
     let delta = time.delta().as_secs_f32();
-    for (speed, direction, mut transform) in &mut query {
+    for (collision_rect, speed, direction, mut transform) in &mut paddles {
         transform.translation.y += direction.y * speed.0 * delta;
+
+        let ray = match direction.y {
+            y if y > 0.0 => Ray2d::new(transform.translation.xy(), Dir2::Y),
+            y if y < 0.0 => Ray2d::new(transform.translation.xy(), Dir2::NEG_Y),
+            _ => continue,
+        };
+
+        for (collision_plane, origin) in walls {
+            if let Some(hit_distance) =
+                ray.intersect_plane(origin.translation.xy(), collision_plane.0)
+                && hit_distance < collision_rect.half_size.y
+            {
+                transform.translation.y =
+                    origin.translation.y + collision_rect.half_size.y * -ray.direction.y;
+                break;
+            }
+        }
     }
 }
 
 fn ball_movement(
     time: Res<Time<Fixed>>,
-    game: Res<Game>,
-    mut query: Query<(&Speed, &CollisionRect, &mut Velocity, &mut Transform), With<Ball>>,
+    walls: Query<(&CollisionPlane, &Transform), (With<Wall>, Without<Ball>)>,
+    paddles: Query<(&CollisionRect, &Transform), (With<Paddle>, Without<Ball>)>,
+    mut balls: Query<(&Speed, &CollisionRect, &mut Velocity, &mut Transform), With<Ball>>,
 ) {
     let delta = time.delta().as_secs_f32();
-    let game_area = game.area;
-    for (speed, collision_rect, mut velocity, mut transform) in &mut query {
+    for (speed, collision_rect, mut velocity, mut transform) in &mut balls {
         transform.translation += velocity.0 * speed.0 * delta;
 
-        let pos = transform.translation.truncate();
-        let half_size = collision_rect.half_size();
-        if pos.y + half_size.y > game_area.top || pos.y - half_size.y < game_area.bottom {
-            velocity.y = -velocity.y;
+        for (wall_collision_plane, wall_transform) in walls {
+            let hit_distance = (wall_transform.translation.y - transform.translation.y).abs();
+            if hit_distance <= collision_rect.half_size.y {
+                let direction = wall_collision_plane.normal.y;
+                transform.translation.y =
+                    wall_transform.translation.y + collision_rect.half_size.y * direction;
+                velocity.0 = velocity.0.reflect(wall_collision_plane.normal.extend(0.0));
+                break;
+            }
         }
-    }
-}
 
-fn clamp_position_to_game_area_top_and_bottom(
-    game: Res<Game>,
-    mut query: Query<(&CollisionRect, &mut Transform)>,
-) {
-    let game_area = game.area;
-    for (collision_rect, mut transform) in &mut query {
-        let pos = transform.translation.truncate();
-        let half_size = collision_rect.half_size();
-        transform.translation.y = pos
-            .y
-            .clamp(game_area.bottom + half_size.y, game_area.top - half_size.y);
-    }
-}
-
-fn bounce_ball_on_paddle(
-    paddle_query: Query<&CollisionRect, With<Paddle>>,
-    mut ball_query: Query<(&CollisionRect, &mut Velocity, &mut Transform), With<Ball>>,
-) {
-    for (ball_collision_rect, mut ball_velocity, mut transform) in &mut ball_query {
-        for paddle_collision_rect in &paddle_query {
-            if !ball_collision_rect.intersects(paddle_collision_rect) {
+        let ball_aabb = Aabb2d::new(transform.translation.xy(), collision_rect.half_size);
+        for (paddle_collision_rect, paddle_transform) in &paddles {
+            let paddle_aabb = Aabb2d::new(
+                paddle_transform.translation.xy(),
+                paddle_collision_rect.half_size,
+            );
+            if !ball_aabb.intersects(&paddle_aabb) {
                 continue;
             }
 
-            let ball_half = ball_collision_rect.half_size();
-            transform.translation.x = if ball_velocity.x > 0.0 {
-                paddle_collision_rect.left - ball_half.x
+            let ball_half = ball_aabb.half_size();
+            transform.translation.x = if velocity.x > 0.0 {
+                paddle_aabb.min.x - ball_half.x
             } else {
-                paddle_collision_rect.right + ball_half.x
+                paddle_aabb.max.x + ball_half.x
             };
 
-            let ball_center = ball_collision_rect.center();
-            let paddle_center = paddle_collision_rect.center();
-            let paddle_half = paddle_collision_rect.half_size();
+            let ball_center = ball_aabb.center();
+            let paddle_center = paddle_aabb.center();
+            let paddle_half = paddle_aabb.half_size();
             let offset = (ball_center.y - paddle_center.y) / paddle_half.y;
             let offset = offset.clamp(-1.0, 1.0);
-            let speed = ball_velocity.length();
-            let new_dir = Vec3::new(-ball_velocity.x.signum(), offset, 0.0).normalize();
-            ball_velocity.0 = new_dir * speed;
+            let speed = velocity.length();
+            let new_dir = Vec3::new(-velocity.x.signum(), offset, 0.0).normalize();
+            velocity.0 = new_dir * speed;
         }
     }
 }
@@ -377,42 +358,37 @@ fn check_score(
     mut commands: Commands,
     mut game: ResMut<Game>,
     score_text_query: Query<Entity, With<ScoreText>>,
-    ball_query: Query<(Entity, &Transform), With<Ball>>,
+    respawns: Query<&Transform, With<RespawnBallArea>>,
+    balls: Query<(Entity, &Transform), With<Ball>>,
 ) {
-    let game_area = game.area;
-    for (entity, transform) in &ball_query {
+    for (entity, transform) in &balls {
         let ball_x = transform.translation.x;
-        let ball_out_left = ball_x < game_area.left;
-        let ball_out_right = ball_x > game_area.right;
-        let ball_inside_area = !ball_out_left && !ball_out_right;
-        if ball_inside_area {
-            continue;
-        }
+        for respawn_transform in &respawns {
+            let respawn_x = respawn_transform.translation.x;
+            let ball_out_left = ball_x < respawn_x && respawn_x < 0.0;
+            let ball_out_right = ball_x > respawn_x && respawn_x > 0.0;
+            let ball_inside_area = !ball_out_left && !ball_out_right;
+            if ball_inside_area {
+                continue;
+            }
 
-        if ball_out_left {
-            game.score.1 += 1;
-        } else if ball_out_right {
-            game.score.0 += 1;
-        }
+            if ball_out_left {
+                game.score.1 += 1;
+            } else if ball_out_right {
+                game.score.0 += 1;
+            }
 
-        commands.entity(entity).insert(NeedsReset);
+            commands.entity(entity).insert(NeedsRespawn);
 
-        for entity in &score_text_query {
-            commands.entity(entity).insert(Dirty);
+            for entity in &score_text_query {
+                commands.entity(entity).insert(Dirty);
+            }
         }
     }
 }
 
-fn update_collision_rect(mut query: Query<(&Transform, &mut CollisionRect)>) {
-    for (transform, mut collision_rect) in &mut query {
-        let center = transform.translation.truncate();
-        let size = collision_rect.size();
-        *collision_rect = CollisionRect(Aabb::new(center, size));
-    }
-}
-
-fn update_speed(game: Res<Game>, mut query: Query<&mut Speed>) {
-    for mut speed in &mut query {
+fn update_speed(game: Res<Game>, mut speeds: Query<&mut Speed>) {
+    for mut speed in &mut speeds {
         speed.0 += game.ball_speed_step;
         if speed.0 > game.ball_max_speed {
             speed.0 = game.ball_max_speed;
@@ -425,7 +401,7 @@ fn main() {
         .insert_resource(ClearColor(Color::BLACK))
         .insert_resource(GameRng(rand::make_rng()))
         .insert_resource(Game {
-            area: Aabb::new(Vec2::ZERO, Vec2::new(800.0, 600.0)),
+            area: Rectangle::new(800.0, 600.0),
             score: (0, 0),
             default_paddle_speed: 300.0,
             default_ball_speed: 100.0,
@@ -440,23 +416,19 @@ fn main() {
             }),
             ..default()
         }))
-        .add_plugins(EguiPlugin::default())
-        .add_plugins(WorldInspectorPlugin::new())
+        .add_plugins((EguiPlugin::default(), WorldInspectorPlugin::new()))
         .add_systems(Startup, setup)
         .add_systems(PreUpdate, (paddle_input, bot_input, close_on_esc))
         .add_systems(Update, update_score_text)
         .add_systems(
             FixedUpdate,
             (
-                reset_ball,
+                respawn_ball,
                 wait_ball_launch_timer,
                 launch_ball,
                 paddle_movement,
                 ball_movement,
-                clamp_position_to_game_area_top_and_bottom,
-                bounce_ball_on_paddle,
                 check_score,
-                update_collision_rect,
                 update_speed,
             )
                 .chain(),
